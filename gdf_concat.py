7444b9284335ab91979c24449e3b8369d9cacc86
shahinmg <shahinmg@ku.edu>
2024-02-14 11:56:52 -0600

    add lagrangain poly sample and gdf concat

---
+++ gdf_concat.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 13 17:40:43 2024
+
+@author: laserglaciers
+"""
+
+import geopandas as gpd
+import os
+
+
+shp_file_path = '/media/laserglaciers/upernavik/its_live_tutorial/KUJ_polygons/'
+os.chdir(shp_file_path)
+gdfs_dates = [(gpd.read_file(fn), gpd.pd.to_datetime(fn[:10].replace('_','-'))) for fn in sorted(os.listdir(shp_file_path)) if fn.endswith('shp')]
+
+gdfs, dates = zip(*gdfs_dates)
+
+rdf = gpd.GeoDataFrame(gpd.pd.concat(gdfs, ignore_index=True), crs=gdfs[0].crs)
+rdf['date'] = dates
+rdf.drop(columns=['col1'], inplace=True)
+rdf.to_crs(crs=None,epsg=3413,inplace=True)
+
+op = '/media/laserglaciers/upernavik/its_live_tutorial/merged_geoms/'
+rdf.to_file(f'{op}kuj_merged.gpkg',driver='GPKG',crs="EPSG:3413")
