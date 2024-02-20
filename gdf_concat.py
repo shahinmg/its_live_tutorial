07e082222de82cc4c6a15739d71347284080ee76
shahinmg <shahinmg@ku.edu>
2024-02-20 14:01:36 -0600

    post meeting 02-20-24

--- gdf_concat.py
+++ gdf_concat.py
@@ -9,17 +9,19 @@ Created on Tue Feb 13 17:40:43 2024
 import geopandas as gpd
 import os
 
+glacier_names = ['KUJ', 'AVA','KAN','EQI']
 
-shp_file_path = '/media/laserglaciers/upernavik/its_live_tutorial/KUJ_polygons/'
-os.chdir(shp_file_path)
-gdfs_dates = [(gpd.read_file(fn), gpd.pd.to_datetime(fn[:10].replace('_','-'))) for fn in sorted(os.listdir(shp_file_path)) if fn.endswith('shp')]
-
-gdfs, dates = zip(*gdfs_dates)
-
-rdf = gpd.GeoDataFrame(gpd.pd.concat(gdfs, ignore_index=True), crs=gdfs[0].crs)
-rdf['date'] = dates
-rdf.drop(columns=['col1'], inplace=True)
-rdf.to_crs(crs=None,epsg=3413,inplace=True)
-
-op = '/media/laserglaciers/upernavik/its_live_tutorial/merged_geoms/'
-rdf.to_file(f'{op}kuj_merged.gpkg',driver='GPKG',crs="EPSG:3413")
+for name in glacier_names:
+    shp_file_path = f'/media/laserglaciers/upernavik/its_live_tutorial/{name}_polygons/'
+    os.chdir(shp_file_path)
+    gdfs_dates = [(gpd.read_file(fn), gpd.pd.to_datetime(fn[:10].replace('_','-'))) for fn in sorted(os.listdir(shp_file_path)) if fn.endswith('shp')] # 
+    
+    gdfs, dates = zip(*gdfs_dates)
+    
+    rdf = gpd.GeoDataFrame(gpd.pd.concat(gdfs, ignore_index=True), crs=gdfs[0].crs)
+    rdf['date'] = dates
+    rdf.drop(columns=['col1'], inplace=True)
+    rdf.to_crs(crs=None,epsg=3413,inplace=True)
+    
+    op = '/media/laserglaciers/upernavik/its_live_tutorial/merged_geoms/'
+    rdf.to_file(f'{op}{name}_merged.gpkg',driver='GPKG',crs="EPSG:3413")
